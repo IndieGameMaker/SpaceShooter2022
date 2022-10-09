f7768b3c2317404f1ee0801fbb2521cacffb7667
Lee JaeHyun <myleje@gmail.com>
2022-10-09 10:18:55 +0900

    .

--- Assets/02_Scripts/PlayerCtrl.cs
+++ Assets/02_Scripts/PlayerCtrl.cs
@@ -9,29 +9,23 @@ public class PlayerCtrl : MonoBehaviour
     private float h = 0.0f;
     private float v = 0.0f;
 
-    // 주석 처리
-    // 가장 먼저 호출(실행)되는 함수
+    // Transform 컴포넌트를 저장하기 위한 변수 선언
+    public Transform tr;
+
     void Start()
     {
-        print(h);
+
     }
 
-    // Update is called once per frame 60, 30 fps
-    // Update 함수 : 화면을 렌더링 하는 주기
-    // 소스코드의 최적화에 주의
     void Update()
     {
-        //h = h + 0.01f;
-        h += 0.01f;
 
-        //v = v + 0.02f;
-        v += 0.02f;
-
-        //h = h - 0.1f;
-        //h -= 0.1f;
-
-        print("h=" + h); // "문자열" + 숫자 => "문자열" + "문자" => "문자열문자"
-        print("v=" + v);
     }
 }
 
+
+
+
+/*
+https://drive.google.com/drive/folders/1ON_4LXOJmg9zYtnaSiasGuCuBGmquuzO?usp=sharing
+*/
