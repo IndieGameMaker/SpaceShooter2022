14782a7d6356542b68e62acfd77cfaaf24ea8382
Lee JaeHyun <myleje@gmail.com>
2022-10-09 15:40:42 +0900

    .

---
+++ Assets/02_Scripts/Fire.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // 총알 프리팹을 저장할 변수
+    public GameObject bulletPrefab;
+    // 총알을 생성할 위치
+    public Transform firePos;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
