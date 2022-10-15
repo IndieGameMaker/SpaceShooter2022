f9fe3c6d6e020d9c62a544c122a627eeb89620f2
Lee JaeHyun <myleje@gmail.com>
2022-10-15 13:44:18 +0900

    .

--- Assets/02_Scripts/Barrel.cs
+++ Assets/02_Scripts/Barrel.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 public class Barrel : MonoBehaviour
 {
     public GameObject expEffect;
+
+    private int hitCount = 0;
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("BULLET"))
+        {
+
+            // 전치 증감연산자
+            ++hitCount; // hitCount += 1; // hitCount = hitCount + 1;
+            if (hitCount == 3)
+            {
+                // 폭발효과 이펙트
+                Instantiate(expEffect, transform.position, transform.rotation);
+            }
+        }
+    }
 }
