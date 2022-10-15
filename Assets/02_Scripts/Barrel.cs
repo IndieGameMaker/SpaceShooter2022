209f258e52613cd632f0f7dbb23b47320d45570d
Lee JaeHyun <myleje@gmail.com>
2022-10-15 14:38:37 +0900

    .

--- Assets/02_Scripts/Barrel.cs
+++ Assets/02_Scripts/Barrel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Barrel : MonoBehaviour
 {
@@ -17,6 +18,16 @@ public class Barrel : MonoBehaviour
     void Start()
     {
         renderer = GetComponentInChildren<MeshRenderer>();
+
+        /*  Random.Range의 난수 발생범위
+            Random.Range(0, 10); 정수일 경우 => 0, 1, 2, ..., 9
+            Random.Range(0.0f, 10.0f); 실수 => 0.0f, .... , 10.0f
+        */
+
+        int index = Random.Range(0, textures.Length); //0, 1, 2
+
+        // 텍스처 교체
+        renderer.material.mainTexture = textures[index];
     }
 
     private void OnCollisionEnter(Collision coll)
