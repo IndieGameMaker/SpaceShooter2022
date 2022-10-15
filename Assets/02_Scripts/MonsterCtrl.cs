1d74e1479e92e3e2ee0373c8315e22b3bc39bc04
Lee JaeHyun <myleje@gmail.com>
2022-10-15 15:36:01 +0900

    .

--- Assets/02_Scripts/MonsterCtrl.cs
+++ Assets/02_Scripts/MonsterCtrl.cs
@@ -13,6 +13,14 @@ public class MonsterCtrl : MonoBehaviour
     // 몬스터의 상태 변수
     public MonsterState state = MonsterState.IDLE;
 
+    // 추적 사정거리
+    [Range(5.0f, 20.0f)]
+    public float traceDist = 10.0f;
+
+    // 공격 사정거리
+    [Range(1.0f, 3.0f)]
+    public float attackDist = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
